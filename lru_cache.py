fe03faf8d7b4a50943ce075d6332cfaf892a2fc9
amit1809 <amitsharma1809@yahoo.com>
2020-05-11 13:27:34 -0700

    Add lru and bloom filter tests

--- lru_cache.py
+++ lru_cache.py
@@ -1,6 +1,5 @@
 from collections import OrderedDict
 
-LRU_CACHE_SIZE = 2
 cache = OrderedDict()
 hash_codes = set()
 
@@ -9,17 +8,17 @@ def lru_cache(cache_size):
     def real_decorator(func):
         def inner(*args, **kw):
             key = args[0]
-            print("Checking LRU cache")
-            if func.__name__ == "get":
+            #print("Checking LRU cache")
+            if(func.__name__ == "get"):
                 if key in cache:
-                    print("In LRU Cache GET")
+                    #print("In LRU Cache GET")
                     cache.move_to_end(key) #move key to the end to make it most recent
                     #print(cache[key])
                     return cache[key]
 
             elif func.__name__ == "put":
                 val = args[1] #val only in the case of PUT
-                print("In LRU Cache PUT")
+                #print("In LRU Cache PUT")
                 cache[key] = val
                 cache.move_to_end(key) #move key to the end to make it most recent
                 if len(cache) > cache_size:
@@ -27,8 +26,23 @@ def lru_cache(cache_size):
 
             elif func.__name__ == "delete":
                 if key in cache:
-                    print("In LRU Cache DELETE")
+                    #print("In LRU Cache DELETE")
                     del cache[key]
+
+
+            elif func.__name__ == "get_data" or func.__name__ == "fibonacci":
+                key = args[0]
+                if key in cache:
+                    print("[cache-hit]" + func.__name__ + "(" + str(key)+") -> "+str(cache[key]))
+                    return cache[key]
+                res = func(key)
+                cache[key] = res
+                cache.move_to_end(key)  # move key to the end to make it most recent
+                if len(cache) > cache_size:
+                    cache.popitem(last=False)  # Delete the least recent used from left
+                print(f"[cache-missed]" + func.__name__ + "(" + str(key)+") -> "+str(cache[key]))
+                return res
+
 
             return func(*args, **kw)
         return inner
--- test_lru_cache.py
+++ test_lru_cache.py
@@ -6,7 +6,6 @@ INVOKE_COUNT = 0
 def fibonacci(n):
     if n < 2:
         return n
-    #print(f"fibonacci {n}: {fibonacci(n-2) + fibonacci(n-1)}")
     return fibonacci(n-2) + fibonacci(n-1)
 
 @lru_cache(4)
